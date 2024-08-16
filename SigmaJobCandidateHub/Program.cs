bdf29a807073f97130a2e3130fb053195867a23d
Raad Smadi <Raad.smadi@menaitech.com>
2024-08-16 23:25:39 +0300

    Apply caching

--- SigmaJobCandidateHub/Program.cs
+++ SigmaJobCandidateHub/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.Converters.Add(new TimeOnlyJsonConverter());
     });
 
+builder.Services.AddMemoryCache();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- SigmaJobCandidateHub/Repository/Services/CandidateRepository.cs
+++ SigmaJobCandidateHub/Repository/Services/CandidateRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using SigmaJobCandidateHub.Data;
 using SigmaJobCandidateHub.Models;
 using SigmaJobCandidateHub.Repository.Interface;
@@ -8,19 +9,38 @@ namespace SigmaJobCandidateHub.Repository.Services
     public class CandidateRepository : ICandidateRepository
     {
         private readonly AppDbContext appDbContext;
+        private readonly IMemoryCache memoryCache;
+        private const string CandidateCacheKeyPrefix = "Candidate_";
 
-        public CandidateRepository(AppDbContext appDbContext)
+        public CandidateRepository(AppDbContext appDbContext, IMemoryCache memoryCache)
         {
             this.appDbContext = appDbContext;
+            this.memoryCache = memoryCache;
         }
 
         public async Task<Candidate> CreateUpdateCandidate(Candidate candidate)
         {
-            var result = await appDbContext.Candidates
-                .SingleOrDefaultAsync(e => e.Id == candidate.Id || e.Email == candidate.Email);
+            // Define cache keys
+            var emailCacheKey = $"{CandidateCacheKeyPrefix}{candidate.Email}";
+
+            Candidate result = memoryCache.Get<Candidate>(emailCacheKey);
+
+            // If not found, query database
+            if (result == null)
+            {
+                result = await appDbContext.Candidates
+                    .SingleOrDefaultAsync(e => e.Id == candidate.Id || e.Email == candidate.Email);
+            }
 
             if (result != null)
             {
+                // Attach entity to the DbContext in case of get result for cache
+                if (appDbContext.Entry(result).State == EntityState.Detached)
+                {
+                    appDbContext.Attach(result);
+                }
+
+                // Update existing candidate
                 result.FirstName = candidate.FirstName;
                 result.LastName = candidate.LastName;
                 result.PhoneNumber = candidate.PhoneNumber;
@@ -32,15 +52,23 @@ namespace SigmaJobCandidateHub.Repository.Services
                 result.Comment = candidate.Comment;
 
                 await appDbContext.SaveChangesAsync();
+
+                // Update cache with the latest data
+                memoryCache.Set(emailCacheKey, result); // Cache by Email
                 return result;
             }
             else
             {
+                // Add new candidate
                 var candidateEntity = await appDbContext.Candidates.AddAsync(candidate);
                 await appDbContext.SaveChangesAsync();
                 candidate.Id = candidateEntity.Entity.Id;
+
+                // Update cache with the new candidate
+                memoryCache.Set(emailCacheKey, candidate); // Cache by Email
                 return candidate;
             }
         }
+
     }
 }
